4fe5b2e66efc87b68c1ecef05a7f1172a0c17a32
koka0630 <youmokakan.220.284@gmail.com>
2021-05-09 01:16:05 +0900

    first commit

---
+++ promise.ts
@@ -0,0 +1,27 @@
+import * as fs from "fs";
+
+function loadJSON(filename: string, callback: (data: any, error:Error) => void){
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            callback(null , err);
+        }
+        else {
+            try {
+                console.log(data.toString());
+               callback (JSON.parse(data.toString()), err);
+            } catch (err) {
+                callback (null, err);
+            }
+        }
+    }
+)};
+
+console.log("start");
+loadJSON("test.json", (data, err) => {
+    if (err === null) {
+        console.log(`works: name:${data.name} age:${data.age}`);
+    } else {
+        console.log(`error: ${err}`);
+    }
+});
+console.log("end");
