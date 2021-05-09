f554e7773bcc5a770bcd7793583d206b8048a512
koka0630 <youmokakan.220.284@gmail.com>
2021-05-09 18:14:32 +0900

    init: test for interface

---
+++ src/interface.ts
@@ -0,0 +1,36 @@
+interface MyInterface{
+    name:string
+}
+
+class MyClass implements MyInterface{
+    name:string;//このメンバーがない場合コンパイルエラーとなる。
+    constructor(name:string){
+        this.name = name
+    }
+}
+
+interface testType {
+    test1: string
+    test2: string
+}
+
+function testFunction (testInput:testType) {
+    return testInput.test1
+}
+
+interface Result {
+    a: number;
+    b: number;
+}
+
+//sum(result: Result)と書くことで引数resultは必ずaとb持たなければならない。
+function sum(result: Result) {
+    return result.a;
+}
+
+var result = {
+    a: 30,
+    b: 20
+};
+
+console.log(sum(result));
